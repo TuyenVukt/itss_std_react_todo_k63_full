68723b2178cd9810ad5f4ead5ac04a53e969467f
TuyenVukt <vutuyenkt2000@mail.com>
2022-06-26 21:33:48 +0700

    Save Todo on Firestore

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,16 +1,107 @@
 import { initializeApp } from "firebase/app";
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/auth';
-import 'firebase/compat/firestore';
+import firebase from 'firebase';
+
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCSaI8gU2tX2Zh8ieL6xUUlBpy01PyynNg",
-  authDomain: "fir-pr-e05d5.firebaseapp.com",
-  projectId: "fir-pr-e05d5",
-  storageBucket: "fir-pr-e05d5.appspot.com",
-  messagingSenderId: "124820285294",
-  appId: "1:124820285294:web:61c5cb7e2447e28899037f"
+  apiKey: "AIzaSyA1uhDs8M8_FenAAJ1xDZPwXBn1NMZzzrM",
+  authDomain: "todo-apps3.firebaseapp.com",
+  projectId: "todo-apps3",
+  storageBucket: "todo-apps3.appspot.com",
+  messagingSenderId: "164759772211",
+  appId: "1:164759772211:web:e4930ad814002957f31746"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const db = firebase.firestore();
+export const auth = firebase.auth();
+export default firebase;
+
+export const getFirebaseItems = async () => {
+  try {
+    const snapshot = await db
+      .collection("todo")
+      .get();
+    const items = snapshot.docs.map(
+      (doc) => ({ ...doc.data(), id: doc.id })
+    );
+    return items;
+  } catch (err) {
+    console.log(err);
+    return [];
+  }
+}
+
+export const addFirebaseItem = async (item) => {
+  try {
+    const todoRef = db.collection("todo");
+    await todoRef.add(item);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export const updateFirebaseItem = async (item, id) => {
+  try {
+    const todoRef = db.collection("todo").doc(id);
+    await todoRef.update(item);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export const clearFirebaseItem = async (item) => {
+  const todoRef = db.collection("todo").doc(item.id);
+  await todoRef.delete().then(function () {
+  }).catch(function (err) {
+    console.log(err);
+  });
+};
+
+export const uiConfig = {
+  signInFlow: 'popup',
+  signInSuccessUrl: "/",
+  signInOptions: [
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+  ],
+}
+
+export const storeUserInfo = async (user) => {
+  const { uid } = user;
+  const userDoc = await db.collection("users").doc(uid).get();
+  if (!userDoc.exists) {
+    await db.collection("users").doc(uid).set({ name: user.displayName });
+    return {
+      name: user.displayName,
+      id: uid,
+    };
+  } else {
+    return {
+      id: uid,
+      ...userDoc.data(),
+    };
+  }
+}
+
+export const updateUser = async (user, image) => {
+  try {
+    const userDoc = await firebase.firestore().collection("users").doc(user.id).get();
+    if (userDoc.exists) {
+      await firebase.firestore().collection("users").doc(user.id).update({ ...userDoc.data(), image: image });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export const uploadImage = async (image) => {
+  const ref = firebase.storage().ref().child(`/images/${image.name}`);
+  let downloadUrl = "";
+  try {
+    await ref.put(image);
+    downloadUrl = await ref.getDownloadURL();
+  } catch (err) {
+    console.log(err);
+  }
+  return downloadUrl;
+};
